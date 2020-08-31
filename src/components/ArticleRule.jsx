847e9a0a1aa352ea29245766e3b2ae7025eaaf69
nterol <n.terol@24sevres.com>
2020-08-31 15:50:46 +0200

    🧹 cleaning things up, gosh all this is old

--- src/components/ArticleRule.jsx
+++ src/components/ArticleRule.jsx
@@ -1,56 +1,55 @@
-import React, {useState, useEffect, useRef} from 'react';
-import styled from 'styled-components';
-import {CustomAnnotation} from './CustomAnnotation';
-
+import React, { useState, useEffect, useRef, createRef } from 'react'
+import styled from 'styled-components'
+import { CustomAnnotation } from './CustomAnnotation'
 
 const SvgContainer = styled.svg`
-    width: 900px;
-    height: 500px;
+  width: 900px;
+  height: 500px;
 `
 
 export function ArticleRule() {
+  const [allWordWidth, setALlWordWidth] = useState([])
+  const sentence = [
+    { word: 'We', annotation: false },
+    { word: 'make', annotation: false },
+    { word: 'the machines', annotation: true },
+    { word: 'run', annotation: true },
+  ]
+  const sentenceRef = Array.from({ length: sentence.length }, () => createRef())
 
-const [allWordWidth, setALlWordWidth] = useState([])
-const sentence = [
-    {word: "We", annotation: false},
-    {word: "make", annotation: false},
-    {word: "the machines", annotation: true },
-    {word: "run", annotation: true}];
-const sentenceRef = Array.from({length: sentence.length}, () => useRef());
+  useEffect(() => {
+    const allWordWidth = sentenceRef.map(ref => ref.current.getBBox().width)
 
-useEffect(() => {
-    const allWordWidth = sentenceRef.map(ref => ref.current.getBBox().width);
-    console.log(allWordWidth);
-    
     const sequentalyCompiledWidth = allWordWidth.reduce((acc, curr, i) => {
-        if (i === 0) acc[i] = curr + 5;
-        else acc[i] = curr + acc[i - 1] + 5;
-        return acc; 
-    }, []);
-    
-    setALlWordWidth(sequentalyCompiledWidth);
-}, [])
+      if (i === 0) acc[i] = curr + 5
+      else acc[i] = curr + acc[i - 1] + 5
+      return acc
+    }, [])
 
-const getWordX = (i) => {
-    if (i === 0 || !allWordWidth.length ) return 0;
-    return allWordWidth[i - 1];
-}
+    setALlWordWidth(sequentalyCompiledWidth)
+  }, [])
 
-    return (
-        <SvgContainer>
-            {sentence.map(({word, annotation}, i) => {
-                const decalX = getWordX(i);
-                if (annotation) console.log(word);
-                return (
-                <g key={word}>
-                    
-                        <text ref={sentenceRef[i]}  x={150 + decalX} y={170}>
-                            {word}
-                        </text>
-                    
-                    {annotation && <CustomAnnotation title={sentence[i].word} x={150 + decalX}/>}
-                </g>
-            )})}    
-        </SvgContainer>
-    )
+  const getWordX = i => {
+    if (i === 0 || !allWordWidth.length) return 0
+    return allWordWidth[i - 1]
+  }
+
+  return (
+    <SvgContainer>
+      {sentence.map(({ word, annotation }, i) => {
+        const decalX = getWordX(i)
+        return (
+          <g key={word}>
+            <text ref={sentenceRef[i]} x={150 + decalX} y={170}>
+              {word}
+            </text>
+
+            {annotation && (
+              <CustomAnnotation title={sentence[i].word} x={150 + decalX} />
+            )}
+          </g>
+        )
+      })}
+    </SvgContainer>
+  )
 }
--- src/components/CustomAnnotation.jsx
+++ src/components/CustomAnnotation.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import {EditableAnnotation, ConnectorLine, Note} from 'react-annotation';
+import React from 'react'
+import { EditableAnnotation, ConnectorLine, Note } from 'react-annotation'
 
-export const CustomAnnotation = ({title, x}) => (
-    <EditableAnnotation
-                x={x}
-                y={170}
-                dy={117}
-                dx={162}
-                color={"#9610ff"}     
-                title={title}
-                label={"yes us Humans"}
-            >
-            <ConnectorLine />
-            <Note 
-                lineType={"vertical"}
-                bgPadding={{"top":15,"left":10,"right":10,"bottom":10}}
-                padding={15}
-                titleColor={"#59039c"} />
-            </EditableAnnotation>
-);
+export const CustomAnnotation = ({ title, x }) => (
+  <EditableAnnotation
+    x={x}
+    y={170}
+    dy={117}
+    dx={162}
+    color={'#9610ff'}
+    title={title}
+    label={'yes us Humans'}
+  >
+    <ConnectorLine />
+    <Note
+      lineType={'vertical'}
+      bgPadding={{ top: 15, left: 10, right: 10, bottom: 10 }}
+      padding={15}
+      titleColor={'#59039c'}
+    />
+  </EditableAnnotation>
+)
