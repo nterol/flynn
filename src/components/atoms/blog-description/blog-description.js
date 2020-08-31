bf0cb95adbe66cbe9bf46fd99c0e9705193b6888
nterol <n.terol@24sevres.com>
2020-08-31 17:25:51 +0200

    💬 I really want to get started this year. Let's go !

---
+++ src/components/atoms/blog-description/blog-description.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import styles from './styles.module.scss';
+
+const BlogDescription = () => {
+  
+  const data = useStaticQuery(graphql`
+    query BlogDescriptionQuery {
+      site {
+        siteMetadata {
+          description
+        }
+      }
+    }
+  `)
+
+console.log("COUCOU", data, styles);
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>{data.site.siteMetadata.description}</div>
+    </section>
+  )
+}
+
+
+export default BlogDescription;
---
+++ src/pages/now.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+function NowPage() {
+  return (
+    <Layout>
+      <main>
+        Hey ! This is my now Page. I discovered it on Maggie Appleton's website
+        and thought it was pretty cool. So these are the things I am doing now:
+        <ul>
+          <li>- Learning about Tarot. </li>
+          <li>- Learning about Gatsby</li>
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export default NowPage
