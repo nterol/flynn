7b5062942bab5234acf6955f2fb58d75de46b14f
Nicolas <nicolas@MBP-de-Nicolas.home>
2020-09-29 01:07:31 +0400

    👷  typo in .mdx and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
             siteMetadata {
               site_url: url
               title
-              description; subtitle
+              description: subtitle
             }
           }
         }`,
